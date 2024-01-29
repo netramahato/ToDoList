abe11ebb91220d886afb401b37d3f36512651518
netra2060002 <np03cs4s210099@heraldcollege.edu.np>
2024-01-29 14:42:37 +0545

    Uploading todo-list app

---
+++ src/component/EditTodoForm.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+
+const TodoForm = ({ editTodo, task }) => {
+  const [value, setValue] = useState("task.task");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    editTodo(value, task.id);
+
+    setValue("");
+  };
+  return (
+    <form className="TodoForm" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="todo-input"
+        value={value}
+        placeholder="update task"
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <button type="submit" className="todo-btn">
+        {" "}
+        update task
+      </button>
+    </form>
+  );
+};
+
+export default TodoForm;
